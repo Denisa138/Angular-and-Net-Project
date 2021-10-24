8c0ce0a10c7f10bcc74042eb23a5fd61d89ae8e0
Denisa138 <stroie_denisa99@yahoo.com>
2021-10-24 19:49:48 +0300

    preview new Add property, group form controls in order to validate it

---
+++ Frontend/src/app/model/iPropertyBase.ts
@@ -0,0 +1,13 @@
+export interface IPropertyBase {
+  Id: number;
+  SellRent: number;
+  Name: string;
+  PType: string;
+  FType: string;
+  Price: number;
+  BHK: number;
+  BuiltArea: number;
+  City: number;
+  RTM: number;
+  Image?:string;
+}
--- Frontend/src/app/property/add-property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property/add-property.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TabsetComponent } from 'ngx-bootstrap/tabs';
-import { IProperty } from '../../IProperty.interface';
+import { IPropertyBase } from 'src/app/model/iPropertyBase';
 
 @Component({
   selector: 'app-add-property',
@@ -16,12 +16,17 @@ export class AddPropertyComponent implements OnInit {
   furnishType: Array<string> = ['Fully', 'Semi', 'Unfurnished'];
   locationType: Array<string> = ['East', 'West', 'South', 'North'];
 
-  propertyView: IProperty = {
+  propertyView: IPropertyBase = {
     Id: null as any,
     Name: '',
     Price: null as any,
     SellRent: null as any,
-    Type: null as any,
+    PType: null as any,
+    FType: null as any,
+    BHK: null as any,
+    BuiltArea: null as any,
+    City: null as any,
+    RTM: null as any
   };
   constructor(private router: Router) { }
 
@@ -34,6 +39,7 @@ export class AddPropertyComponent implements OnInit {
 
   onSubmit(){
     console.log(this.addPropertyForm);
+    console.log(this.addPropertyForm.value.BuiltInfo.SellRent);
   }
 
   selectTab(tabId: number){
